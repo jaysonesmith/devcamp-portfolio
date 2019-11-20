699b0ce92aea43078257503fe9571a0850ed8748
jaysonesmith <gh@nes.33mail.com>
2019-11-20 12:48:01 -0700

    Added new and edit buttons for portolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -27,7 +27,7 @@ class PortfoliosController < ApplicationController
 
   def update
     @portfolio_item = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
